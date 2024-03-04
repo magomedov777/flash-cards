65e052ec348cbbce920c7ddf0af301229e9d10f0
Arkadiy <alibullterrier@gmail.com>
2024-03-04 17:53:05 +0200

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
+      <Button onClick={() => (window.location.href = 'https://google.com')}>hello</Button>
+      <Button as={'a'} href={'https://google.com'}>
+        Hi
+      </Button>
+    </div>
+  )
 }
