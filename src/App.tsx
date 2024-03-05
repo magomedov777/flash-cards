1a5b6b339f0c11e94fe1c9cd7d70668d70e768af
andreikostsen <pks@tut.by>
2024-03-05 13:35:46 +0300

    feat: change button title

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export function App() {
     <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
       <Button onClick={() => (window.location.href = 'https://google.com')}>hello</Button>
       <Button as={'a'} href={'https://google.com'}>
-        Hi
+        Hi From Andrei
       </Button>
     </div>
   )
