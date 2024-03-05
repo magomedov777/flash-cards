b39066ada57746af4b784765b6ca96eac709422f
Olga Genier <genier.developer@gmail.com>
2024-03-05 18:18:12 +0500

    add Typography component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
 
 export function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
-      <Button onClick={() => (window.location.href = 'https://google.com')}>hello</Button>
-      <Button as={'a'} href={'https://google.com'}>
-        Hi
-      </Button>
+      <Typography.H1 ml={10} mt={20}>
+        Heading 1
+      </Typography.H1>
+      <Typography.H2 ml={10} mt={20}>
+        Heading 2
+      </Typography.H2>
+      <Typography.H3 ml={10} mt={20}>
+        Heading 3
+      </Typography.H3>
     </div>
   )
 }
