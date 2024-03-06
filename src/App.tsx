d884cb10a8e9a2be40ade5fe17c2a5f680e1207e
Olga Genier <genier.developer@gmail.com>
2024-03-06 21:10:21 +0500

    change typography component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { Typography } from '@/components/ui/typography';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography1'
 
 export function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
-      <Typography.H1 ml={10} mt={20}>
-        Heading 1
-      </Typography.H1>
-      <Typography.H2 ml={10} mt={20}>
-        Heading 2
-      </Typography.H2>
-      <Typography.H3 ml={10} mt={20}>
-        Heading 3
-      </Typography.H3>
+      <Typography variant={'h1'}>H1</Typography>
+      <Typography variant={'body1'}>body1</Typography>
       <Button onClick={() => (window.location.href = 'https://google.com')}>hello</Button>
       <Button as={'a'} href={'https://google.com'}>
         Hi From Andrei
       </Button>
     </div>
-  );
+  )
 }
