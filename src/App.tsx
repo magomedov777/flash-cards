fae6148acc2e51996a9925909b4d72b05f25f4ba
Olga Genier <genier.developer@gmail.com>
2024-03-07 18:08:48 +0500

    change typography component completely

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import { Button } from '@/components/ui/button'
-
 import { Typography } from './components/ui/typography'
 
 export function App() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '12px' }}>
-      <Typography variant={'h1'}>H1</Typography>
-      <Typography variant={'body1'}>body1</Typography>
-      <Button onClick={() => (window.location.href = 'https://google.com')}>hello</Button>
-      <Button as={'a'} href={'https://google.com'}>
-        Hi From Andrei
-      </Button>
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <Typography as={'h1'} variant={'h1'}>
+        H1
+      </Typography>
+      <Typography as={'h2'} variant={'h2'}>
+        H2
+      </Typography>
+      <Typography as={'h3'} variant={'h3'}>
+        H3
+      </Typography>
+      <Typography as={'h4'} variant={'h4'}>
+        H4
+      </Typography>
+      <Typography as={'body'} variant={'body1'}>
+        body1
+      </Typography>
+      <Typography as={'body'} variant={'body2'}>
+        body2
+      </Typography>
+      <Typography as={'a'} variant={'link1'}>
+        link1
+      </Typography>
+      <Typography as={'a'} variant={'link2'}>
+        link2
+      </Typography>
+      <Typography as={'p'} variant={'caption'}>
+        Caption
+      </Typography>
+      {/*<Button onClick={() => (window.location.href = 'https://google.com')}>hello</Button>*/}
+      {/*<Button as={'a'} href={'https://google.com'}>*/}
+      {/*  Hi From Andrei*/}
+      {/*</Button>*/}
     </div>
   )
 }
--- src/components/ui/typography/typography.stories.tsx
+++ src/components/ui/typography/typography.stories.tsx
@@ -1,28 +1,92 @@
-import { Meta } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
-import { Typography } from './typography'
+import { Typography } from '@/components/ui/typography/typography'
 
-export default {
+const meta = {
   component: Typography,
-  title: 'Components/Typography',
-} as Meta<typeof Typography>
+} satisfies Meta<typeof Typography>
 
-export const AllTypography = () => {
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Typography>Heading 1</Typography>
-      <Typography>Heading 2</Typography>
-      <Typography>Heading 3</Typography>
-      <Typography>Subtitle 1</Typography>
-      <Typography>Subtitle 2</Typography>
-      <Typography>Body 1</Typography>
-      <Typography>Body 2</Typography>
-      <Typography>Overline</Typography>
-      <Typography>Caption</Typography>
-      <Typography>Caption Link</Typography>
-      <Typography>Subtitle Link</Typography>
-      <Typography>Link</Typography>
-      <Typography>Error</Typography>
-    </div>
-  )
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Typography_H1: Story = {
+  args: {
+    children: 'Header1',
+    disabled: false,
+    variant: 'h1',
+  },
+}
+
+export const Typography_H2: Story = {
+  args: {
+    children: 'Header2',
+    disabled: false,
+    variant: 'h2',
+  },
+}
+export const Typography_H3: Story = {
+  args: {
+    children: 'Header3',
+    disabled: false,
+    variant: 'h3',
+  },
+}
+export const Typography_H4: Story = {
+  args: {
+    children: 'Header4',
+    disabled: false,
+    variant: 'h4',
+  },
+}
+
+export const Typography_Body1: Story = {
+  args: {
+    children: 'Body1',
+    disabled: false,
+    variant: 'body1',
+  },
+}
+export const Typography_Body2: Story = {
+  args: {
+    children: 'Body2',
+    disabled: false,
+    variant: 'body2',
+  },
+}
+
+export const Typography_Link1: Story = {
+  args: {
+    children: 'Link1',
+    disabled: false,
+    variant: 'link1',
+  },
+}
+export const Typography_Link2: Story = {
+  args: {
+    children: 'Link2',
+    disabled: false,
+    variant: 'link2',
+  },
+}
+export const Typography_Subtitle1: Story = {
+  args: {
+    children: 'Subtitle1',
+    disabled: false,
+    variant: 'subtitle1',
+  },
+}
+export const Typography_Subtitle2: Story = {
+  args: {
+    children: 'Subtitle2',
+    disabled: false,
+    variant: 'subtitle2',
+  },
+}
+export const Typography_Overline: Story = {
+  args: {
+    children: 'Overline',
+    disabled: false,
+    variant: 'overline',
+  },
 }
