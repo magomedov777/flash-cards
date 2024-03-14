271befedd9ee025d1c841c796f51d6f86e56235d
andreikostsen <pks@tut.by>
2024-03-14 17:03:19 +0300

    fix: checkbox focus

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ReactNode, useState } from 'react'
 
 import * as Checkbox from '@radix-ui/react-checkbox'
 import { CheckIcon } from '@radix-ui/react-icons'
@@ -6,30 +6,32 @@ import { CheckIcon } from '@radix-ui/react-icons'
 import s from './checkbox.module.scss'
 
 type PropsType = {
-  defaultChecked: boolean
+  children?: ReactNode
+  defaultChecked?: boolean
+  disabled?: boolean
 }
 
-export const CheckboxDemo = (props: PropsType) => {
-  const [checked, setChecked] = useState<boolean>(props.defaultChecked)
-
-  console.log(checked)
+export const CheckboxDemo = ({ children, defaultChecked = false }: PropsType) => {
+  const [checked, setChecked] = useState<boolean>(defaultChecked)
 
   return (
     <form>
-      <Checkbox.Root
-        className={
-          checked ? `${s.CheckboxRoot} ${s.Selected}` : `${s.CheckboxRoot} ${s.Unselected}`
-        }
-        defaultChecked={checked}
-        onClick={event => setChecked(prevState => !prevState)}
-      >
-        <Checkbox.Indicator className={s.CheckboxIndicator}>
-          <CheckIcon className={s.CheckboxIcon} />
-        </Checkbox.Indicator>
-      </Checkbox.Root>
-      <button onClick={() => setChecked(true)} type={'button'}>
-        Toggle indeterminate
-      </button>
+      <div className={s.Container}>
+        <div className={s.Ellipse}>
+          <Checkbox.Root
+            className={
+              checked ? `${s.CheckboxRoot} ${s.Selected}` : `${s.CheckboxRoot} ${s.Unselected}`
+            }
+            defaultChecked={checked}
+            onClick={() => setChecked(prevState => !prevState)}
+          >
+            <Checkbox.Indicator className={s.CheckboxIndicator}>
+              <CheckIcon className={s.CheckboxIcon} />
+            </Checkbox.Indicator>
+          </Checkbox.Root>
+        </div>
+        <label className={s.Label}>{children}</label>
+      </div>
     </form>
   )
 }
